f9103e5b8aab79cf1f38d5daef7e3b97836b8870
Umar Ahmed <wspuma@gmail.com>
2019-01-31 17:43:14 -0500

    added auto clicking

--- wordblitz.cc
+++ wordblitz.cc
@@ -1,3 +1,14 @@
+#include <X11/Xlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
 #include <string>
 #include <iostream>
 #include <vector>
@@ -9,7 +20,7 @@
 using namespace std;
 
 
-const vector<vector <int>> dir = {{0, 1}, {1, 1}, {1, 0}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {0, -1}};
+const vector<vector <int>> DIREC = {{0, 1}, {1, 1}, {1, 0}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {0, -1}};
 
 
 // For square matrices
@@ -41,7 +52,7 @@ string vectorToString(const vector<string> &s) {
 
 void getPath(const vector<vector<string>> &matrix, const int i, const int j,
         set <string> &words, vector<string> current = {}) {
-    if (current.size() > 7) {
+    if (current.size() > 6) { // TODO remove hardcoded upper bound
         return;
     }
     if (!in_matrix(i, j, matrix.size())) {
@@ -50,7 +61,7 @@ void getPath(const vector<vector<string>> &matrix, const int i, const int j,
     if (count(current.begin(), current.end(), matrix[i][j])) {
         return;
     }
-    for (auto &x: dir) {
+    for (auto &x: DIREC) {
         current.emplace_back(matrix[i][j]);
         words.insert(vectorToString(current));
         getPath(matrix, i + x[0], j + x[1], words, current);
@@ -70,7 +81,54 @@ string getWord(const string s) {
 }
 
 
-set<string> getAllWords(vector<vector<string>> &matrix) {
+// Click at position given by button
+void mouseClick(int button)
+{
+    Display *display = XOpenDisplay(NULL);
+
+    XEvent event;
+
+    if(display == NULL)
+    {
+        fprintf(stderr, "Cannot initialize the display\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&event, 0x00, sizeof(event));
+
+    event.type = ButtonPress;
+    event.xbutton.button = button;
+    event.xbutton.same_screen = True;
+
+    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+
+    event.xbutton.subwindow = event.xbutton.window;
+
+    while(event.xbutton.subwindow)
+    {
+        event.xbutton.window = event.xbutton.subwindow;
+
+        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+    }
+
+    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+
+    XFlush(display);
+
+    usleep(100000);
+
+    event.type = ButtonRelease;
+    event.xbutton.state = 0x100;
+
+    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+
+    XFlush(display);
+
+    XCloseDisplay(display);
+}
+
+
+void getAllWords(vector<vector<string>> &matrix, set<string> * dict) {
     set<string> words;
     make_unique(matrix);
     getPath(matrix, 0, 0, words);
@@ -82,11 +140,11 @@ set<string> getAllWords(vector<vector<string>> &matrix) {
     set<string> clean_words;
     for (auto i: words) {
         string word = getWord(i);
-        if (word.size() > 3) {
-            clean_words.insert(word);
+        if (dict->count(word) > 0) {
+            std::cout << i << endl;
+            //play(i);
         }
     }
-    return clean_words;
 }
 
 
@@ -95,14 +153,14 @@ void print_matrix(const vector<vector<string>> &matrix) {
         for (int j = 0; j < 4; ++j) {
             cout << matrix[i][j];
         }
-        cout << endl;
+        std::cout << endl;
     }
 }
 
 
 int main() {
     // Load dictionary into set
-    ifstream fin {"dict.txt"};
+    ifstream fin {"dict2.txt"};
     set<string> dict;
     string word = "";
     while (fin >> word) {
@@ -120,16 +178,5 @@ int main() {
         input_matrix.emplace_back(current);
         current.clear();
     }
-    set<string> words = getAllWords(input_matrix);
-    int k = 0;
-    for (auto &i: words) {
-        if (dict.find(i) != dict.end()) {
-            cout << i << "  ";
-            k += 1;
-            if (k % 8 == 0) {
-                cout << endl;
-            }
-        }
-    }
+    getAllWords(input_matrix, &dict);
 }
-
